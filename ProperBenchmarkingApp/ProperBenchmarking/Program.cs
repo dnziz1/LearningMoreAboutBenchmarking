98ba4f85015f0ad99d85dded07b194b0c716c0e9
Daniel <danielnkurunziza@gmail.com>
2025-08-21 16:07:33 +0100

    Adding files to project folder

---
+++ ProperBenchmarkingApp/ProperBenchmarking/Program.cs
@@ -0,0 +1,37 @@
+﻿//using BenchmarkDotNet.Attributes;
+//using BenchmarkDotNet.Running;
+//using System.Text;
+
+//namespace ProperBenchmarking;
+
+//[MemoryDiagnoser]
+
+//public class Program
+//{
+//    int iterations = 1_000;
+//    public static void Main()
+//    {
+//        var summary = BenchmarkRunner.Run<Program>();
+//    }
+
+//    [Benchmark]
+//    public void SlowTool()
+//    {
+//        string name = "";
+//        for (int i = 0; i < iterations; i++)
+//        {
+//            name += i;
+//        }
+//    }
+
+//    [Benchmark]
+//    public void QuickTool()
+//    {
+//        StringBuilder sb = new StringBuilder();
+//        for (int i = 0; i < iterations; i++)
+//        {
+//            sb.Append(i);
+//        }
+//    }
+//}
+//// When benchmarking switch debug to release mode 
